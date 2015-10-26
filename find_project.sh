9ce34b6ef122fcb6654f667f8cc1e2221d0ad172
lixiao123 <beijing.2008.lm@163.com>
2015-10-26 21:55:20 +0800

    修改代码实现逻辑

--- find_project.sh
+++ find_project.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash -x
 
 ### Description: 查找给定地址的服务器的projects
 ### Author: foree
@@ -7,45 +7,52 @@
 #定义变量
 SERVER_PATH_1=~/server/bringup36
 SERVER_PATH_2=~/server/bringup38
+SERVERS_PATH=(~/server/bringup36/ ~/server/bringup38/)
 PROJECT_LIST=""
 PROJECT_CONF=~/.config/foree-tools/foree-tools.conf
 
-#遍历服务器下的目录
-function find_dir
-{
-    cd $SERVER_PATH_1
-    root_dir_list=$( find . -maxdepth 1 -type d |cut -d / -f 2 |grep -v '^\.' )
-    for dir in $root_dir_list
-    do
-        is_project_dir $dir
-    done
-    echo "SERVER_1:$PROJECT_LIST" >>$PROJECT_CONF
-    PROJECT_LIST=""
-    cd $SERVER_PATH_2
-    root_dir_list=$( find . -maxdepth 1 -type d |cut -d / -f 2 |grep -v '^\.' )
-    for dir in $root_dir_list
-    do
-        is_project_dir $dir
-    done
-    echo "SERVER_2:$PROJECT_LIST" >>$PROJECT_CONF
-}
-
-#判断给定的目录是否是project 目录,如果不是，继续向下查找
+#判断给定的目录是否是project根目录
 function is_project_dir
 {
-    root_dir=$1
-    if [ -d $root_dir/.repo ];then
-        echo "project dir is $root_dir"
-        PROJECT_LIST=${PROJECT_LIST}" "${root_dir}
+    project_dir=$1
+
+    if [ -d $project_dir/.repo ];then
+        echo "project dir is $project_dir"
+        PROJECT_LIST=$PROJECT_LIST" "$project_dir
     else
-        dir_level_2=$( find $root_dir -maxdepth 1 -type d )
-        for dir in $dir_level_2
-        do
-            if [ -d $dir/.repo ];then
-                echo "project dir is $dir"
-                PROJECT_LIST=${PROJECT_LIST}" "${dir}
-            fi
-        done
+        return 2
     fi
 }
-find_dir
+
+#遍历服务器下的目录
+function find_project_dir
+{
+    cd $SERVER_PATH_1
+    level1_dir_list=$( find . -maxdepth 1 -type d |cut -d / -f 2 |grep -v '^\.' )
+    for dir in $level1_dir_list
+    do
+        is_project_dir $dir
+        if [ $? -eq '2' ];then
+            cd $dir
+            level2_dir_list=$( find . -maxdepth 1 -type d |cut -d / -f 2 |grep -v '^\.' )
+            for level2_dir in $level2_dir_list
+            do
+                is_project_dir $level2_dir
+                if [ $? -eq '2' ];then
+                    cd $level2_dir
+                    level3_dir_list=$( find . -maxdepth 1 -type d |cut -d / -f 2 |grep -v '^\.' )
+                    for level3_dir in $level3_dir_list
+                    do
+                        is_project_dir $level3_dir
+                    done
+                    cd ..
+                fi
+            done
+            cd ..
+        fi
+    done
+
+    echo "SERVER_1:$PROJECT_LIST" >>$PROJECT_CONF
+}
+
+find_project_dir
