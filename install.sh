5694c4530a52bdab484a2090a9f82c635d9bd592
lixiao123 <beijing.2008.lm@163.com>
2015-11-10 09:43:34 +0800

    修改使用统一的配置文件名称路径

--- install.sh
+++ install.sh
@@ -2,10 +2,8 @@
 ### Description: 自己的工具集合的安装脚本
 ### Author: foree
 ### Date: 20151025
-TOOLS_NAME="foree-tools"
-TOOLS_PATH=~/bin
-TOOLS_CONFIG_DIR=~/.config/$TOOLS_NAME
-TOOLS_CONFIG_NAME="foree-tools.conf"
+
+source ~/.config/foree-tools/foree-tools.conf
 
 #创建程序的配置目录
 if [ ! -d $TOOLS_CONFIG_DIR ];then
