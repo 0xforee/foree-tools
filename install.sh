46e3a56735cc9d23eb8c9b7bab342873affac3e4
lixiao123 <beijing.2008.lm@163.com>
2015-11-12 21:26:30 +0800

    为无法构建project list的用户增加默认的list文件

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@
 ### Author: foree
 ### Date: 20151025
 
-source ~/.config/foree-tools/foree-tools.conf
+source ./foree-tools.conf.default
+
+FUNCTION_LIST="common.sh fastboot_flash.sh pick_branch.sh ssh_bringup.sh fadb_funtion find_project.sh"
 
 #创建程序的配置目录
 if [ ! -d $TOOLS_CONFIG_DIR ];then
@@ -13,17 +15,24 @@ fi
 #初始化或者更新配置文件
 if [ ! -f $TOOLS_CONFIG_DIR/$TOOLS_CONFIG_NAME -o ./foree-tools.conf.default -nt $TOOLS_CONFIG_DIR/$TOOLS_CONFIG_NAME ];then
     cp ./foree-tools.conf.default $TOOLS_CONFIG_DIR/$TOOLS_CONFIG_NAME
-    echo "init/update success"
+    if [ $? -eq '0' ];then echo "init/update success";fi
 fi
 
 #生成存放project的对应文件
-source $TOOLS_CONFIG_DIR/$TOOLS_CONFIG_NAME
-
 for i in ${!BRINGUP_PROJECT_LIST[@]}
 do
-    echo ${BRINGUP_PROJECT_LIST[$i]}
     if [ "${BRINGUP_PROJECT_LIST[$i]}"x = "SERVER_$i"x -a ! -f $TOOLS_CONFIG_DIR/SERVER_$i ];then
-        touch $TOOLS_CONFIG_DIR/SERVER_$i
+        echo "Installing ${BRINGUP_PROJECT_LIST[$i]}......"
+        cp ./SERVER_${i}_default $TOOLS_CONFIG_DIR/SERVER_$i
+    fi
+done
+
+#初始化或者更新相关文件
+for function_list_file in $FUNCTION_LIST
+do
+    if [ ! -f $TOOLS_CONFIG_DIR/$function_list_file -o ./$function_list_file -nt $TOOLS_CONFIG_DIR/$function_list_file ];then
+        echo "Installing $function_list_file ......."
+        cp ./$function_list_file $TOOLS_CONFIG_DIR/$function_list_file
     fi
 done
 
@@ -34,10 +43,10 @@ else
     whattime=`date +%Y_%m%d_%H%M`
     cat >>~/.bashrc <<EOF
 # $whattime add by foreetools
-source ~/github/foree-tools/fadb_funtion
-source ~/github/foree-tools/ssh_bringup.sh
-source ~/github/foree-tools/find_project.sh
-source ~/github/foree-tools/common.sh
+source ~/.config/foree-tools/fadb_funtion
+source ~/.config/foree-tools/ssh_bringup.sh
+source ~/.config/foree-tools/find_project.sh
+source ~/.config/foree-tools/common.sh
 EOF
 echo "add success"
 fi
